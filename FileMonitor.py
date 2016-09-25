36cc7e34482980d1382cb5428ac02c3415b1c06b
Roy Touw <roytouw@hotmail.com>
2016-09-25 18:18:31 +0200

    map_new method added

--- FileMonitor.py
+++ FileMonitor.py
@@ -5,14 +5,8 @@ from FileWriter import FileWriter
 
 
 class FileMonitor:
-    # check directory structure
-    # binary set all files
-    # keep polling all root folders and files to compare binary value
-    # if binary of one changes
-    # locate change file
-    # call FileWriter on it
-    # register new binary value
-    # get back to polling all root folders and files to compare binary values
+    # For mapping the files in the starting directory(place called from)
+    # For monitoring changes in the mapped files.
 
     def __init__(self):
         print('Mapping directory.')
@@ -31,12 +25,30 @@ class FileMonitor:
                 result[os.path.join(path, file)] = os.path.getmtime(os.path.join(path, file))
         return result
 
+    # Map newly added files to directory
+    def map_new(self, dir=None, path=os.getcwd()):
+        if dir is None:
+            dir = self.directory
+        for file in os.listdir(path):
+            if os.path.join(path, file) not in dir:     # New file detected.
+                time.sleep(1)
+                print('New file detected.', os.path.join(path, file))
+                if os.path.isdir(os.path.join(path, file)):     # Newly detected file is a subdirectory.
+                    dir[os.path.join(path, file)] = \
+                        ({}, os.path.getmtime(os.path.join(path, file)))
+                    self.map_new(dir[os.path.join(path, file)], os.path.join(path, file))
+                else:
+                    try:
+                        dir[os.path.join(path, file)] = os.path.getmtime(os.path.join(path, file))
+                    except FileNotFoundError:   # Temporary file detected, should be ignored.
+                        continue
+
     # Keep polling files for changes.
     def poll_changes(self):
-        # self.substitute_flags()     # Check all files initially.
         while True:
             self.scan_directory(self.directory)  # Start with scanning the root directory.
             time.sleep(1)
+            self.map_new()      # Check if new files where added during the polling.
 
     # Substitute all given flags in root and sub directories on changes.
     def substitute_flags(self, dir=None):
@@ -50,13 +62,19 @@ class FileMonitor:
 
     # Scan a directory for changes, recursive call on sub directories.
     def scan_directory(self, dir):
+        temp = []
         for key, value in dir.items():
             if isinstance(value, tuple):  # If it's registered as a subdirectory.
                 self.scan_directory(value[0])
             else:
-                if value != os.path.getmtime(key):
-                    self.fileWriter.reg_replace_all(key)
-                    dir[key] = os.path.getmtime(key)
+                try:
+                    if value != os.path.getmtime(key):
+                        self.fileWriter.reg_replace_all(key)
+                        dir[key] = os.path.getmtime(key)
+                except FileNotFoundError:   # Temporary file detected, place on list for removal.
+                    temp.append(key)
+        for key in temp:    # Remove all detected temporary files.
+            dir.pop(key)
 
     # Print the directory recursive.
     def print_directory(self, dir=None):
@@ -72,5 +90,8 @@ class FileMonitor:
 
 # Main testing method.
 if __name__ == "__main__":
-    fileMonitor = FileMonitor()
-    fileMonitor.print_directory()
+    print('Testing method is empty.')
+    # fileMonitor = FileMonitor()
+    # fileMonitor.print_directory()
+    # fileMonitor.map_new()
+    # fileMonitor.print_directory()
